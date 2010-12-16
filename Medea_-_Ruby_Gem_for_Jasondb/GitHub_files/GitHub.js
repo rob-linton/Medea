fdda8ed10613f57d7c54bdf7f5c675e5f2260d65
Rob Linton <robl@jasondb.com>
2010-12-16 15:05:53 +1100

    Initial web site

---
+++ Medea_-_Ruby_Gem_for_Jasondb/GitHub_files/GitHub.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20101216
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('GitHub_files/GitHubMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupAllIEPNGBGs();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
