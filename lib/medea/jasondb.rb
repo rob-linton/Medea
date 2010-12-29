6c3c8331a36f0b6e87153c77a57a78c6632f07f4
Michael Jensen <michaelj@jasondb.com>
2010-12-29 11:13:19 +1100

    Rolling back my test/dev change.

--- lib/medea/jasondb.rb
+++ lib/medea/jasondb.rb
@@ -1,24 +1,9 @@
 module JasonDB
-  def JasonDB::class_exists? class_name
-    const = Module.const_get(class_name)
-    return const.is_a? Class
-  rescue NameError
-    return false
-  end
-
   #jason_url here doesn't include the http[s]:// part, but does include the domain and a trailing '/'
   #( so it's "rest.jasondb.com/<domain>/" )
   def JasonDB::db_auth_url mode=:secure
-    #check to see if this is a Rails environment
-    if class_exists? "Rails"
-      config = Rails.configuration.database_configuration[Rails.env]
-    else
-      #if not, use some defaults for testing medea.
-      config = {"user" => "michael",
-                "topic" => "medea-test",
-                "password" => "password"}
-    end
-
+    config = Rails.configuration.database_configuration[Rails.env]
+    
     user = config["user"]
     topic = config["topic"]
     password = config["password"]
