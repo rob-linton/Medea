f044e6cfb101e785f0711f2f95c3bd6ff466c9e0
Michael Jensen <michaelj@jasondb.com>
2011-01-07 13:42:23 +1100

    Changed how config files are read. First uses ENV and then Rails.configuration.database_configuration
    Required keys:
      jason_user
      jason_password
      jason_topic
    
    Optional:
      jason_host	//some time in the future, JasonDB may offer access via different hosts

--- lib/medea/jasondb.rb
+++ lib/medea/jasondb.rb
@@ -2,11 +2,17 @@ module JasonDB
   #jason_url here doesn't include the http[s]:// part, but does include the domain and a trailing '/'
   #( so it's "rest.jasondb.com/<domain>/" )
   def JasonDB::db_auth_url mode=:secure
-    config = Rails.configuration.database_configuration[Rails.env]
+    if ENV.include? "jason_user"
+      config = ENV
+    elsif defined? Rails
+      config = Rails.configuration.database_configuration[Rails.env]
+    else
+      raise "No configuration for JasonDB found!"
+    end
     
-    user = config["user"]
-    topic = config["topic"]
-    password = config["password"]
+    user = config["jason_user"]
+    topic = config["jason_topic"]
+    password = config["jason_password"]
     if config["jason_host"]
       host = config["jason_host"]
     else
--- lib/medea/version.rb
+++ lib/medea/version.rb
@@ -1,3 +1,3 @@
 module Medea
-  VERSION = "0.3.5"
+  VERSION = "0.3.6"
 end
